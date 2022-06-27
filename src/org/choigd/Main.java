e4d1eedfb3546690b629a6af84197b999f8e9874
choigd0307@gmail.com <choigd0307@gmail.com>
2022-06-27 19:53:47 +0900

    Big O Notation

--- src/org/choigd/Main.java
+++ src/org/choigd/Main.java
@@ -2,6 +2,26 @@ package org.choigd;
 
 public class Main {
 
+    /*
+    Big O Notation(빅오 표기법)
+    시간 복잡도, 공간 복잡도
+    O(1) = 상수
+    O(N) = n값이 증가(for문 시에 max,min값이 없을경우)
+    0(N^2) = 이중 for문
+
+    배열 빅오 메소드
+    push - O(1)
+    pop - O(1)
+    sort - O(N*log N)
+    shift - O(N)
+    나머지 - O(N)
+
+
+
+    */
+
+
+
     public static void main(String[] args) {
 	// write your code here
     }
