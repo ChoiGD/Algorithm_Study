b78b399781383074086b874fb2ead4267eb33894
ChoiGD <ChoiGD0307@Gmail.com>
2022-07-03 20:05:22 +0900

    Search Algorithm

--- src/org/choigd/study2/RecursiveFunction.java
+++ src/org/choigd/study2/RecursiveFunction.java
@@ -1,2 +1,54 @@
-package org.choigd.study2;public class RecursiveFunction {
+package org.choigd.study2;
+/*
+Recursive Function(재귀함수)
+- 재귀 : 자기 자신을 호출하는 함수
+- 반복문 대신하여서 재귀함수를 사용할 때 코드가 깔끔해진다
+- 모든 곳에 사용된다
+
+Call Stack (호출 스택)
+- 호출 스택은 자바스크립트의 보이지 않는 곳에서 작동하는 정적 데이터 구조
+- 진행중인 함수 위에 함수가 올라감
+- 기본 케이스 : 재귀가 끝난 상태
+
+JS코드 문제예시
+
+function power(num1,num2){
+
+    const res = Math.pow(num1,num2)
+
+    return res
+
+}
+
+function factorial(num){
+
+   if(num === 0){return 1}
+
+   return num * factorial(num-1)
+
+}
+
+function productOfArray(arr){
+
+    //곱하기니까 기본값은 1
+    let sum = 1;
+
+    for(let i=0; i<arr.length; i++){
+        sum *= arr[i]
+    }
+
+    return sum
+}
+
+
+function recursiveRange(num){
+
+    if(num === 0){return 0}
+
+   return num + recursiveRange(num-1)
+}
+
+ */
+public class RecursiveFunction {
+
 }
---
+++ src/org/choigd/study2/SearchAlgorithm.java
@@ -0,0 +1,34 @@
+package org.choigd.study2;
+
+/*
+Search Algorithm(검색 알고리즘)
+
+1. Linear Search(선형 검색) O(1), O(N)
+첫 부분에서 시작해서 끝부분으로 이동하면서 한번에 하나의 항목을 확인할 수 도 있다.
+ -indexOf
+ -includes
+ -find
+ -findIndex
+
+function linearSearch(arr, num){
+  //기본적 선형 구조
+
+  let idx = -1
+  for(let i=0; i<arr.length; i++){
+      if(arr[i] === num){
+          idx = i
+      }
+  }
+  return idx
+
+}
+
+
+2. Binary Search(이진 검색)
+
+
+*/
+
+
+public class SearchAlgorithm {
+}
