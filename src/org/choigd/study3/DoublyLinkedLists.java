0e921a8b4002a0b0cbe75bef9bac804f020820b6
choigd0307@gmail.com <choigd0307@gmail.com>
2022-07-11 16:35:39 +0900

    set 메소드

--- src/org/choigd/study3/DoublyLinkedLists.java
+++ src/org/choigd/study3/DoublyLinkedLists.java
@@ -95,6 +95,15 @@ class DoublyLinkedList{
         }
         return current;
     }
+    //set 메소드
+    set(index, val){
+        var foundNode = this.get(index);
+        if(foundNode != null){
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
 
 }
 
