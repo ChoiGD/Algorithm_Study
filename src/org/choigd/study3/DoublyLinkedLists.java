c5e6e4925a89ea8830aa45e3fffad050b67cd0a6
choigd0307@gmail.com <choigd0307@gmail.com>
2022-07-11 15:00:00 +0900

    DoublyLinkedLists + Node + constructor 생성

--- src/org/choigd/study3/DoublyLinkedLists.java
+++ src/org/choigd/study3/DoublyLinkedLists.java
@@ -1,8 +1,20 @@
 package org.choigd.study3;
 /*
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
 
-
-
+class DoublyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
 
  */
 public class DoublyLinkedLists {
