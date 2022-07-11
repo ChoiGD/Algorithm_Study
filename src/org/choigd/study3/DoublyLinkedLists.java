6e2ea30ed659134f40c6afa3741abca707cdb0ed
choigd0307@gmail.com <choigd0307@gmail.com>
2022-07-11 15:38:29 +0900

    shift 메소드

--- src/org/choigd/study3/DoublyLinkedLists.java
+++ src/org/choigd/study3/DoublyLinkedLists.java
@@ -44,6 +44,21 @@ class DoublyLinkedList{
         this.length--;
         return poppedNode;
     }
+    //shift 메소드
+    shift(){
+        if(this.length === 0) return undefined;
+        var oldHead = this.head;
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+    }
 }
 
  */
