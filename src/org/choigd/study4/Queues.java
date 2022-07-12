eac399d516e78aca397086db5f307e18f9335aa8
choigd0307@gmail.com <choigd0307@gmail.com>
2022-07-12 16:42:59 +0900

    Queues class + enqueue

--- src/org/choigd/study4/Queues.java
+++ src/org/choigd/study4/Queues.java
@@ -12,20 +12,37 @@ Queues
 
 클래스
 
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next =null;
+    }
+
+}
+
 class Queue{
     constructor(){
         this.first = null;
         this.last = null;
         this.size = 0;
     }
-}
+    enqueue(val){
+        var newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        }else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
+    }
+    dequeue(){
 
-class Node{
-    constructor(value){
-        this.value = value;
-        this.next =null;
     }
 }
+
+
 
  */
 public class Queues {
