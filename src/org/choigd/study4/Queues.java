979dc9bc0bf8859591b3d6504b7256f2dd257a48
choigd0307@gmail.com <choigd0307@gmail.com>
2022-07-12 16:29:38 +0900

    Queues 정리

---
+++ src/org/choigd/study4/Queues.java
@@ -0,0 +1,13 @@
+package org.choigd.study4;
+/*
+Queues
+- 선입선출, FIFO 자료구조, 먼저 들어간것이 먼저 나온다
+- Background tasks
+- Uploading resources
+- Printing / Task processing
+
+배열 + 클래스로 구성 가능
+
+ */
+public class Queues {
+}
