f19378a818a70cef6fe9a8d1226165d0a5d223cf
choigd0307@gmail.com <choigd0307@gmail.com>
2022-07-12 15:48:41 +0900

    Stack

---
+++ src/org/choigd/study4/Stack.java
@@ -0,0 +1,13 @@
+package org.choigd.study4;
+/*
+Stack(스택)
+- 후입선출, LIFO의 자료구조
+- 가장 나중에 추가 된 것이 먼저 삭제 된다.
+- 함수 호출을 다루는 상황에서 사용되는 것
+- Undo / Redo [ex) 컨트롤 + z, 실행 취소]
+- Routing (the history object)
+
+
+ */
+public class Stack {
+}
