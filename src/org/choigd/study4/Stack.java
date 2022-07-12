1d48c46734dde93f335b08bd59ae8965e2474f5a
choigd0307@gmail.com <choigd0307@gmail.com>
2022-07-12 15:58:56 +0900

    Stack 개념

--- src/org/choigd/study4/Stack.java
+++ src/org/choigd/study4/Stack.java
@@ -7,7 +7,9 @@ Stack(스택)
 - Undo / Redo [ex) 컨트롤 + z, 실행 취소]
 - Routing (the history object)
 
-
+배열도 스택을 사용할 수 있다.
+가장 먼저 추가된 것이 가장 나중에 제거되고, 가장 나중에 추가된 것이 가장 먼저 제거되도록 하려면
+push, pop, unshift, shift를 이용하자
  */
 public class Stack {
 }
