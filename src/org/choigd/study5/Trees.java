ecd979a0f47ae9f0019507ca1314fab141a53434
choigd0307@gmail.com <choigd0307@gmail.com>
2022-07-13 15:04:03 +0900

    Binary Search Trees

--- src/org/choigd/study5/Trees.java
+++ src/org/choigd/study5/Trees.java
@@ -14,6 +14,17 @@ Trees(이진 검색 트리)
 - Artificial Intelligence
 - Folders in Operating Systems
 - Computer File Systems
+
+트리 종류
+- Trees
+- Binary Trees : 자식 노드가 0~2개 여야만 한다
+- Binary Search Trees : BST, 데이터를 비교해서 정렬이 가능하다(크고 작은..), 이진 트리의 특별한 종류, 부모 노드의 왼쪽은 부모노므보다 작고 오른쪽은 부모보다 크다
+
+**Binary Search Trees**
+- 속도가 빠르다
+- 트리를 반으로 나눠 작업하는 것이 되기에...
+
+
  */
 public class Trees {
 }
