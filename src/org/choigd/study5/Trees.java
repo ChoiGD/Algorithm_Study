547ae752cba915a47ab78336ce1860b13a143fda
choigd0307@gmail.com <choigd0307@gmail.com>
2022-07-13 16:12:57 +0900

    Binary Search Trees + Find 메소드

--- src/org/choigd/study5/Trees.java
+++ src/org/choigd/study5/Trees.java
@@ -66,6 +66,27 @@ class BinarySearchTree{
             }
         }
     }
+    //find 메소드
+    find(value){
+        if(this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        if(!found) return undefined;
+        return current;
+    }
+
+
+
+
 }
 
  */
