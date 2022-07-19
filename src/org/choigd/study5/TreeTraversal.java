e686296de1f359f45ff32aa5cc47a16df4bb1005
choigd0307@gmail.com <choigd0307@gmail.com>
2022-07-19 16:40:10 +0900

    Breadth First Search(너비 우선 탐색)

--- src/org/choigd/study5/TreeTraversal.java
+++ src/org/choigd/study5/TreeTraversal.java
@@ -1,7 +1,99 @@
 package org.choigd.study5;
 /*
 TreeTraversal(트리 순회)
-- 너비 우선 탐색
+어떤 트리에서든지 사용할 수 있는 개념
+재귀함수를 많이 사용하여 해결한다
+
+- Breadth First Search(너비 우선 탐색)
+수평으로 작업을 한다
+1. 큐를 사용하여 선입선출
+2. 요소를 맨 앞에서 제거하고 맨 뒤에다가 넣어야
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        var newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }
+        var current = this.root;
+        while(true){
+            if(value === current.value) return undefined;
+            if(value < current.value){
+                if(current.left === null){
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if(current.right === null){
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }
+        }
+    }
+    find(value){
+        if(this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        if(!found) return undefined;
+        return current;
+    }
+    contains(value){
+        if(this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+    //Breadth First Search(너비 우선 탐색)
+    BFS(){
+        var node = this.root,
+            data = [],
+            queue = [];
+        queue.push(node);
+
+        while(queue.length){
+           node = queue.shift();
+           data.push(node.value);
+           if(node.left) queue.push(node.left);
+           if(node.right) queue.push(node.right);
+        }
+        return data;
+    }
+}
+
+
 - 깊이 우선 탐색
     - 전위 순회
     - 후위 순회
