fa034a54da879bfb7c6f5263d62fc6fa99066a66
ChoiGD <ChoiGD0307@Gmail.com>
2022-07-24 21:57:09 +0900

    Heap MaxBinaryHeap

--- src/org/choigd/study6/Heaps.java
+++ src/org/choigd/study6/Heaps.java
@@ -7,8 +7,30 @@ Heap(힙) or Binary Heaps(이진 힙)
 2. 부모노드의 키값과 자식노드의 키값 사이에는 대소관계가 성립
     2-1. 키값 대소관계는 오로지 부모자식 간에만 성립, 형제사이에는 대소관계까 정해지지 않음
 
+class MaxBinaryHeap {
+    constructor(){
+        this.values = [];
+    }
+    //추가 메소드
+    insert(element){
+        this.values.push(element);
+        this.bubbleUp();
+    }
 
-
+    bubbleUp(){
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while(idx > 0){
+            let parentIdx = Math.floor((idx - 1)/2);
+            let parent = this.values[parentIdx];
+            if(element <= parent) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+}
+let heap = new MaxBinaryHeap();
  */
 
 public class Heaps {
