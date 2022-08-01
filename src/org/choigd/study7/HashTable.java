5a2ed6b114a5b415b6ec6b391979e9e7ae6f9e40
ChoiGD <ChoiGD0307@Gmail.com>
2022-08-01 20:58:46 +0900

    improved hash function

--- src/org/choigd/study7/HashTable.java
+++ src/org/choigd/study7/HashTable.java
@@ -27,6 +27,18 @@ function hash(key, arrayLen) {
   return total;
 }
 
+//개선된 해시함수
+function hash(key, arrayLen) {
+  let total = 0;
+  let WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
+    let value = char.charCodeAt(0) - 96
+    total = (total * WEIRD_PRIME + value) % arrayLen;
+  }
+  return total;
+}
+
 */
 public class HashTable {
 }
