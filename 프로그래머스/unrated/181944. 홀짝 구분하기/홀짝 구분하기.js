dc783c73f5ff9e622867c1e87fef60c6c6f90ef1
Choi GyoDeuk <82931725+ChoiGD@users.noreply.github.com>
2023-04-25 20:00:20 +0900

    [unrated] Title: 홀짝 구분하기, Time: 45.44 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    n = Number(input[0]);
+    
+    const evenOdd = (n) => {
+        if(n % 2 === 0){
+        return `${n} is even`;
+    }else{
+        return `${n} is odd`;
+        }
+    }
+    
+    console.log(evenOdd(n))
+    
+});
