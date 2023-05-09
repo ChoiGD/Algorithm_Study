7bbfa478b31eee90e0cfb7eedded98618c610bda
Choi GyoDeuk <82931725+ChoiGD@users.noreply.github.com>
2023-05-09 22:10:55 +0900

    [unrated] Title: 주사위 게임 3, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.js
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.js
@@ -1,36 +1,18 @@
 function solution(a, b, c, d) {
-    const dices = [a, b, c, d]
-    const numFrequency = dices.reduce((acc, cur) => {
-        acc[cur] = acc[cur] ?? 0
-        acc[cur]++
-        return acc
-    }, {})
+
+    const [p, q, r, s] = [a, b, c, d].sort((a, b) => a - b);
     
-    const valInfo = Object.values(numFrequency)
-    const keyInfo = Object.keys(numFrequency)
-    // return valInfo
-    const countVal = Math.max(...valInfo)
-    switch(countVal) {
-        case 4:
-            return a*1111
-        case 3: {
-            const threeKey  = keyInfo.find((a) => numFrequency[a] === 3)
-            const oneKey = keyInfo.find((a) => numFrequency[a] === 1)
-            console.log(threeKey, oneKey)
-            return (10 * Number(threeKey) + Number(oneKey))**2
-        }
-        case 2: {
-            if(keyInfo.length === 2) {
-                if(a === b) return (a+c) * Math.abs(a-c)
-                return (a+b) * Math.abs(a-b)
-            } else {
-                return  keyInfo.reduce((acc, cur) => numFrequency[cur] === 1 ? acc*cur : acc, 1)
-            }
-        }   
-        default: {
-            return Math.min(a,b,c,d)
-        }
-    }
+    if (p === q && q === r && r  === s) return 1111 * a;
     
-    return numFrequency
+    if (p === q && q === r) return Math.pow(10 * p + s, 2);
+    if (q === r && r === s) return Math.pow(10 * q + p, 2);
+    
+    if (p === q && r === s) return (p + r) * Math.abs(p - r);
+
+   
+    if (p === q) return r * s;
+    if (q === r) return p * s;
+    if (r === s) return p * q;
+
+    return Math.min(p, q, r, s);
 }
