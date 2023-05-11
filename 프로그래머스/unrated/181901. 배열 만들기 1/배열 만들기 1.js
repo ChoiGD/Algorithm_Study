5877a9269b8ec8e562c1343e8e255c3e34620596
Choi GyoDeuk <82931725+ChoiGD@users.noreply.github.com>
2023-05-11 21:39:20 +0900

    [unrated] Title: 배열 만들기 1, Time: 6.57 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.js
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.js
@@ -1,11 +1,8 @@
 function solution(n, k) {
     var answer = [];
-    let kNum = [];
-    
     for(let i=1; i<=n; i++){
         answer.push(i)
     }
-    
     
     return answer.filter(num => num%k === 0);
 }
