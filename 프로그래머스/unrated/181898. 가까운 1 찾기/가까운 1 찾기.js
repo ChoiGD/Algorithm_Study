41496c5ee286acd346ea6ad64dc806eb6f49205e
Choi GyoDeuk <82931725+ChoiGD@users.noreply.github.com>
2023-05-11 22:26:50 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -0,0 +1,14 @@
+function solution(arr, idx) {
+    var answer = 0;
+    
+    for(let i=0; i<arr.length; i++){
+        if(i >= idx && arr[i]===1){
+            answer = i;
+            break;
+        } else{
+            answer = -1;
+        }
+    }
+    
+    return answer;
+}
