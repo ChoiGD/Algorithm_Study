8b37c811bba8eecf43b000ded04b5fca940e9c79
Choi GyoDeuk <82931725+ChoiGD@users.noreply.github.com>
2023-05-13 17:48:51 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,22 @@
+function solution(num_list) {
+    var answer = 0;
+    
+    let odd=0;
+    let even=0;
+    
+    for(let i=0; i<num_list.length; i++){
+        if(i%2===0){
+            even+=num_list[i]
+        }else{
+            odd+=num_list[i]
+        }
+    }
+    if(odd<even){
+        answer=even;
+    }else if(odd==even){
+        answer=odd;
+    }else{
+        answer=odd;
+    }
+    return answer;
+}
