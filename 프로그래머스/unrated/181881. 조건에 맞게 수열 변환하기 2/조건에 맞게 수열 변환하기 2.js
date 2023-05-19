230f627e6ca07363b7548c46833528563478c41f
Choi GyoDeuk <82931725+ChoiGD@users.noreply.github.com>
2023-05-19 22:32:48 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -1,15 +1,66 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+function isEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+}
+
 function solution(arr) {
-    let cnt = 0;
-    
-    while(!arr.every(num => (num >= 50 && num % 2 === 1) || (num < 50 && num % 2 === 0))) {
-        arr = arr.map(num => {
-            if (num >= 50 && num % 2 === 0) return num / 2;
-            if (num < 50 && num % 2 === 1) return num * 2 + 1;
-            return num;
-        })
-        
-        cnt++;
-    };
-    
-    return cnt;
+    let answer = 0;
+
+    while (true) {
+        let transformedArr = arr.map(num => {
+            if (num >= 50 && num % 2 === 0) {
+                return num / 2;
+            } else if (num < 50 && num % 2 === 1) {
+                return num * 2 + 1;
+            } else {
+                return num;
+            }
+        });
+
+        if (isEqual(arr, transformedArr)) {
+            break;
+        }
+
+        arr = transformedArr;
+        answer++;
+    }
+
+    return answer;
 }
