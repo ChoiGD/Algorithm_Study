36d45e067572420338c68f70db2fd673744d353d
Choi GyoDeuk <82931725+ChoiGD@users.noreply.github.com>
2023-05-22 20:54:31 +0900

    [unrated] Title: 배열 만들기 6, Time: 3.71 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,18 @@
+function solution(arr) {
+    let stk= [];
+    
+    for(let i=0; i<arr.length; i++){
+        if(stk.length===0){
+            stk.push(arr[i])
+            
+        }else if(stk.length!==0 && stk[stk.length-1] === arr[i]){
+            stk.pop()
+            
+        }else if(stk.length!==0 && stk[stk.length-1] !== arr[i]){
+            stk.push(arr[i])
+        }
+    }
+    (stk.length===0) ? stk.push(-1) : stk 
+    
+    return stk;
+}
