73405a506fdd07dae2a89e665eaf00e633eb60f4
Choi GyoDeuk <82931725+ChoiGD@users.noreply.github.com>
2023-05-23 21:01:10 +0900

    [unrated] Title: 무작위로 K개의 수 뽑기, Time: 0.78 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -1,4 +1,21 @@
 function solution(arr, k) {
-    const set = new Set(arr);
-    return set.size < k ? [...set, ...Array(k - set.size).fill(-1)] : [...set].slice(0, k);
+    let answer = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (answer.length === k) {
+            return answer;
+        }
+        if (answer.includes(arr[i])) {
+            continue;
+        } else {
+            answer.push(arr[i]);
+        }
+    }
+    let test = answer.length;
+
+    for (let i = 0; i < k - test; i++) {
+
+        answer.push(-1);
+      
+    }
+    return answer;
 }
