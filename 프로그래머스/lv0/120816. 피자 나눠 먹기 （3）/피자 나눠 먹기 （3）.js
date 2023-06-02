574b762c7b10626111b3908b1df600ece430f296
Choi GyoDeuk <82931725+ChoiGD@users.noreply.github.com>
2023-06-02 22:10:48 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -0,0 +1,11 @@
+function solution(slice, n) {
+    let answer = 0;
+    
+    if(n % slice ===0){
+        answer = Math.floor(n / slice)
+    }else{
+        answer = Math.floor(n / slice +1)
+    }
+    
+    return answer;
+}
