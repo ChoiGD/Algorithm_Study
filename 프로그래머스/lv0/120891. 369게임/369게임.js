e4dad84d05b48e4a46d61b6d58ceb673fd6224c5
Choi GyoDeuk <82931725+ChoiGD@users.noreply.github.com>
2023-08-28 22:06:23 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -0,0 +1,13 @@
+function solution(order) {
+    let answer = 0;
+    let str = order.toString();
+    
+    for(let i=0; i<str.length; i++){
+        if(str[i]==3 || str[i]==6 || str[i]==9){
+            answer+=1;
+        }
+    }
+    
+    
+    return answer;
+}
